49b0559a8723ba91cbaeec2a99b762703a340767
TyHafen <tyhafen@gmail.com>
2022-08-17 09:42:57 -0700

    commit

--- base/views.py
+++ base/views.py
@@ -1,15 +1,13 @@
-
 from django.shortcuts import render
-
 from .models import Room
 
 
 # Create your views here.
-rooms = [
-    {'id': 1, 'name': 'Lets learn python'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend Developers'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets learn python'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend Developers'},
+# ]
 
 
 def home(request):
@@ -19,9 +17,11 @@ def home(request):
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
