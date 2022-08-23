4ad2907c2bc00b4fb4771bc52b486cd3dec547e2
TyHafen <tyhafen@gmail.com>
2022-08-23 10:58:40 -0700

    commit

--- studybud/settings.py
+++ studybud/settings.py
@@ -133,13 +133,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'static')
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, "/staticfiles/")
+MEDIA_URL = '/images/'
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
